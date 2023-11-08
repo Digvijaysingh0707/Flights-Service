93be88c1f8d95f096a63f4f1fe9053ee59483cf7
Digvijay Singh <Digvijaysinghonly1@gmail.com>
2023-11-08 09:46:18 +0530

    create airplane service

---
+++ src/controllers/airplane-controller.js
@@ -0,0 +1,8 @@
+const { AirplaneService } = require("../services");
+const { StatusCodes } = require("http-status-codes");
+
+async function createAirplane(req, res) {}
+
+module.exports = {
+  createAirplane,
+};
